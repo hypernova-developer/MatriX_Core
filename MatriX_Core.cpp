132fd52402edbd23d9822a82a99c08eef6be03aa
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-04-23 21:37:13 +0300

    Update MatriX_Core.cpp

--- MatriX_Core.cpp
+++ MatriX_Core.cpp
@@ -48,7 +48,7 @@ std::string getBestMove(std::string moves)
 
 void handleGame(std::string gameId, bool amIWhite)
 {
-    std::cout << "[GAME-CONNECT] Entering room: " << gameId << std::endl << std::flush;
+    std::cout << "[GAME-CONNECT] Target ID: " << gameId << std::endl << std::flush;
     std::string cmd = "curl -s -N --no-buffer -H \"Authorization: Bearer " + TOKEN + "\" \"https://lichess.org/api/bot/game/stream/" + gameId + "\"";
     FILE* pipe = popen(cmd.c_str(), "r");
     if (!pipe)
@@ -75,7 +75,9 @@ void handleGame(std::string gameId, bool amIWhite)
             size_t spaces = 0;
             if(!allMoves.empty())
             {
-                spaces = std::count(allMoves.begin(), allMoves.end(), ' ') + 1;
+                std::stringstream ss(allMoves);
+                std::string temp;
+                while(ss >> temp) spaces++;
             }
             
             bool myTurn = (amIWhite && (spaces % 2 == 0)) || (!amIWhite && (spaces % 2 != 0));
@@ -88,6 +90,7 @@ void handleGame(std::string gameId, bool amIWhite)
                 }
             }
         }
+        
         if (line.find("\"status\"") != std::string::npos && line.find("\"started\"") == std::string::npos)
         {
             break;
@@ -95,12 +98,12 @@ void handleGame(std::string gameId, bool amIWhite)
     }
     pclose(pipe);
     currentMatchesCount--;
-    std::cout << "[SYSTEM] Game over: " << gameId << std::endl << std::flush;
+    std::cout << "[SYSTEM] Game ended: " << gameId << std::endl << std::flush;
 }
 
 void streamEvents()
 {
-    std::cout << "[DEBUG] Awaiting events..." << std::endl << std::flush;
+    std::cout << "[DEBUG] Matrix Awaiting..." << std::endl << std::flush;
     std::string cmd = "curl -s -N --no-buffer -H \"Authorization: Bearer " + TOKEN + "\" \"https://lichess.org/api/stream/event\"";
     FILE* pipe = popen(cmd.c_str(), "r");
     if (!pipe)
@@ -112,30 +115,28 @@ void streamEvents()
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
     {
         std::string line(buffer);
-        if (line.length() < 5)
-        {
-            continue;
-        }
+        if (line.length() < 5) continue;
 
         if (line.find("\"type\":\"challenge\"") != std::string::npos)
         {
-            size_t idStart = line.find("\"id\":\"") + 6;
-            std::string c_id = line.substr(idStart, line.find("\"", idStart) - idStart);
+            size_t idPos = line.find("\"id\":\"") + 6;
+            std::string c_id = line.substr(idPos, line.find("\"", idPos) - idPos);
             
             if (line.find("muhammedeymengurbuz") != std::string::npos || line.find("Muhammedeymengurbuz") != std::string::npos)
             {
-                std::cout << "[AUTH] Owner recognized. Accepting..." << std::endl << std::flush;
+                std::cout << "[AUTH] King recognized. Accepting..." << std::endl << std::flush;
                 std::string acceptCmd = "curl -s -X POST \"https://lichess.org/api/challenge/" + c_id + "/accept\" -H \"Authorization: Bearer " + TOKEN + "\"";
                 system(acceptCmd.c_str());
             }
         }
         else if (line.find("\"type\":\"gameStart\"") != std::string::npos)
         {
-            size_t gamePos = line.find("\"game\":");
-            size_t idPos = line.find("\"id\":\"", gamePos) + 6;
-            std::string g_id = line.substr(idPos, line.find("\"", idPos) - idPos);
+            size_t gameSection = line.find("\"game\":{");
+            size_t idKey = line.find("\"id\":\"", gameSection) + 6;
+            std::string g_id = line.substr(idKey, line.find("\"", idKey) - idKey);
             
             bool white = (line.find("\"color\":\"white\"") != std::string::npos);
+            
             if (currentMatchesCount < 1)
             {
                 currentMatchesCount++;
@@ -148,11 +149,8 @@ void streamEvents()
 
 int main()
 {
-    if (TOKEN.empty())
-    {
-        return 1;
-    }
-    std::cout << "[DEPLOY] MatriX_Core v7.7 True-ID Online." << std::endl << std::flush;
+    if (TOKEN.empty()) return 1;
+    std::cout << "[DEPLOY] MatriX_Core v7.8 Allman-Hard-Logic Online." << std::endl << std::flush;
     while (true)
     {
         streamEvents();
